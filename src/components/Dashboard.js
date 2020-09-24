839029e5a1ed7deb138e0ba39431eb7e7e25b17b
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-24 15:55:36 -0500

    created basic dashboard with the logout button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Dashboard() {
-  return (<div>Dashboard</div>);
+function Dashboard(props) {
+  const handleLogut = () => {
+    props.history.push('/login');
+  };
+
+  return (
+    <div>
+      Welcome User
+      <br />
+      <br />
+      <button
+        type="button"
+        onClick={handleLogut}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
+
+Dashboard.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Dashboard;
