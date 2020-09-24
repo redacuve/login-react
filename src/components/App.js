34d308572fb90010ad5a39807dae4bddd8f1c3f0
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-24 14:42:09 -0500

    added basic scaffold for the components on the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <nav>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </nav>
+        <div className="container mx-auto">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
