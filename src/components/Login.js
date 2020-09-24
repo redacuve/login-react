0f4908890c72ae6cd8a201e50591c73fe4782863
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-24 15:41:00 -0500

    created basic login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,77 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import PropTypes from 'prop-types';
 
-function Login(){
-  return(<div>Login</div>)
+function Login(props) {
+  const { register, handleSubmit, errors } = useForm();
+
+  const handleLogin = () => {
+    props.history.push('/dashboard');
+  };
+
+  return (
+    <form
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      onSubmit={handleSubmit(handleLogin)}
+    >
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+          name="username"
+          ref={register({ required: true })}
+        />
+        {errors.username && (
+          <span className="text-red-500 text-xs italic">
+            This field is required
+          </span>
+        )}
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          name="password"
+          placeholder="******************"
+          ref={register({ required: true })}
+        />
+        {errors.password && (
+          <span className="text-red-500 text-xs italic">
+            This field is required
+          </span>
+        )}
+      </div>
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Sign In
+        </button>
+      </div>
+    </form>
+  );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
