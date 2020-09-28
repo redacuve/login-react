5beb31724c83d53511a655cfe92bcae9aa557622
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-28 17:47:39 -0500

    update App.js to use public and private Route

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   BrowserRouter, Route, Switch,
 } from 'react-router-dom';
+import PrivateRoute from '../Utils/PrivateRoute';
+import PublicRoute from '../Utils/PublicRoute';
 import Dashboard from './Dashboard';
 import Home from './Home';
 import Login from './Login';
@@ -15,8 +17,8 @@ function App() {
         <div className="container max-w-xs mx-auto">
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/dashboard" component={Dashboard} />
+            <PublicRoute path="/login" component={Login} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
           </Switch>
         </div>
       </BrowserRouter>
